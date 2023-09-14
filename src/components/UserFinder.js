bd0c9539ae95813a51215f178f6dc7abfeb75ea3
Gabriel Sollero <gabriel@electricfish.co>
2023-09-13 23:07:33 -0300

    Adding error boundaries for Users component.

---
+++ src/components/UserFinder.js
@@ -0,0 +1,55 @@
+import { Component, Fragment, useState, useEffect } from "react";
+
+import ErrorBoundary from "./ErrorBoundary";
+import Users from "./Users";
+import UsersContext from "../store/users-context";
+import classes from "./UserFinder.module.css";
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      filteredUsers: this.context.users,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </Fragment>
+    );
+  }
+}
+
+export default UserFinder;
