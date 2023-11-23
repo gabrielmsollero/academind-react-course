9207e272b58f94fe87fcc27f96a17c81bc0ad132
Gabriel Sollero <gabriel@electricfish.co>
2023-11-23 08:41:51 -0300

    Adding Cart and Checkout modals.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext"
 
 import Button from "./UI/Button";
 import logoImg from "../assets/logo.jpg";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
   const totalCartItems = cartCtx.items.reduce((total, item) => {
     return total + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -17,7 +24,7 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button onClick={handleShowCart} textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
