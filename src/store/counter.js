3f799078894ea97c532e3035ec6f5727221f06de
Gabriel Sollero <gabriel@electricfish.co>
2023-12-01 10:37:57 -0300

    Uploading final version.

---
+++ src/store/counter.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      /* Using redux toolkit, direct changes to the
+       * state object are allowed because the state
+       * variable passed to this function is already
+       * a copy of the actual state object
+       */
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export const counterActions = counterSlice.actions;
+export default counterSlice.reducer;
+
