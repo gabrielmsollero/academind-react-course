42a77e2c923aabba7ab208d1edbcd682f5ade741
Gabriel Sollero <gabriel@electricfish.co>
2023-12-05 12:08:55 -0300

    Creating cart slice and finishing initial implementation.

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui";
+import cartSlice from "./cart";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { 
+    ui: uiSlice.reducer,
+    cart: cartSlice.reducer
+  },
 });
 
 export default store;
