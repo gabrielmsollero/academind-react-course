a39794bb68ec5a313127533e514028593cb5fa0b
Gabriel Sollero <gabriel@electricfish.co>
2023-12-05 21:14:11 -0300

    Uploading final version.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import ErrorPage from "./pages/Error";
+import RootLayout from "./pages/Root";
+import ProductDetailPage from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> }, // path: ''
+      {
+        path: "products",
+        element: <ProductsPage />,
+      },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
