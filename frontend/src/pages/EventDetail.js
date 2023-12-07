7161826646d43d1cdbfe61180fc09ef595cf3e3d
Gabriel Sollero <gabriel@electricfish.co>
2023-12-07 20:20:26 -0300

    Solving initial challenges.

---
+++ frontend/src/pages/EventDetail.js
@@ -0,0 +1,10 @@
+import {useParams} from 'react-router-dom';
+
+export default function EventDetailPage() {
+  const params = useParams();
+
+  return <>
+    <h1>Event Detail</h1>
+    <p>Event ID: {params.id}</p>
+  </>
+};
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Event 1'
+  },
+  {
+    id: 'e2',
+    title: 'Event 2'
+  }
+];
+
+export default function EventsPage() {
+  return <>
+    <h1>Events</h1>
+    <ul>
+      {DUMMY_EVENTS.map(event => <li key={event.id}><Link to={event.id}>{event.title}</Link></li>)}
+    </ul>
+  </>
+};
