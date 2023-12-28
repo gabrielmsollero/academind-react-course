17f22e81aaff04010449c322653fbd3822631724
Gabriel Sollero <bielsollero2@gmail.com>
2023-12-28 09:57:39 -0300

    Finalizing Greeting test suite.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,6 +1,16 @@
+import { useState } from "react"
+import Output from "./Output";
+
 export default function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  }
+
   return <div>
     <h2>Hello World!</h2>
-    <p>It's good to see you!</p>
+    {changedText ? <Output>Changed!</Output> : <Output>It's good to see you!</Output>}
+    <button onClick={changeTextHandler}>Change Text!</button>
   </div>
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -12,5 +13,38 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders only initial message when button has NOT been clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // nothing
+
+    // Assert
+    const goodToSeeYouElement = screen.getByText("It's good to see you", { exact: false });
+    const changedElement = screen.queryByText("Changed", { exact: false });
+    
+    expect(goodToSeeYouElement).toBeInTheDocument();
+    expect(changedElement).not.toBeInTheDocument(); // or expect(changedElement).toBeNull()
+  });
+
+  test("renders 'Changed!' when button has been clicked", () => {
+    // Arrange
+    render(<Greeting />);
+    
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const goodToSeeYouElement = screen.queryByText("It's good to see you", {
+      exact: false,
+    });
+    const changedElement = screen.getByText("Changed", { exact: false });
+
+    expect(goodToSeeYouElement).not.toBeInTheDocument();
+    expect(changedElement).toBeInTheDocument();
   });
 });
