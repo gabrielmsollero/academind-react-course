923218460778bf011553580dca875dc95e8f97b0
Gabriel Sollero <bielsollero2@gmail.com>
2023-12-30 15:07:01 -0300

    Adding Todo class.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,14 @@
+import Todo from "../models/todo";
+
 type Props = {
-  items: string[];
+  items: Todo[];
 };
 
 export default function Todos({ items }: Props) {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
